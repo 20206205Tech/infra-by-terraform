df0bbc2a13b3d62904e04c46b53004dfda8f9ba0
VuVanNghia <vuvannghia.work@gmail.com>
2026-03-04 11:28:40 +0700

    feat: add Neon module and provider configuration

---
+++ modules/neon/main.tf
@@ -0,0 +1,9 @@
+resource "neon_project" "this" {
+  for_each = toset(var.projects)
+
+  org_id    = var.doppler_secrets_map["NEON_ORGANIZATION_ID"]
+  name      = each.key
+  region_id = "aws-ap-southeast-1"
+
+  history_retention_seconds = 21600
+}
---
+++ modules/neon/variables.tf
@@ -0,0 +1,18 @@
+variable "doppler_secrets_map" {
+  type        = map(string)
+  sensitive   = true
+  description = "Doppler secrets passed from root module"
+}
+
+variable "projects" {
+  type        = list(string)
+  description = "Danh sách tên các project Neon cần tạo"
+  default = [
+    # "dev-api-gateway",
+    # "prod-api-gateway",
+    # "dev-api-gateway-http-log",
+    # "prod-api-gateway-http-log",
+    "dev-be-ai",
+    "prod-be-ai",
+  ]
+}
