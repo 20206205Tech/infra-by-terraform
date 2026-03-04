df4bf8a4a58fd81ef4210bbe0e52e69af5dd903e
VuVanNghia <vuvannghia.work@gmail.com>
2026-03-04 13:19:45 +0700

    feat: add supabase_settings resource for authentication configuration

--- modules/supabase/main.tf
+++ modules/supabase/main.tf
@@ -20,3 +20,18 @@ data "supabase_pooler" "this" {
   for_each    = supabase_project.this
   project_ref = each.value.id
 }
+
+resource "supabase_settings" "this" {
+  for_each = supabase_project.this
+
+  project_ref = each.value.id
+
+  auth = jsonencode({
+    external_google_enabled   = true
+    external_google_client_id = var.doppler_secrets_map["GOOGLE_CLIENT_ID"]
+    external_google_secret    = var.doppler_secrets_map["GOOGLE_CLIENT_SECRET"]
+
+    site_url                 = "https://20206205.tech"
+    additional_redirect_urls = ["https://20206205.tech/auth/callback"]
+  })
+}
