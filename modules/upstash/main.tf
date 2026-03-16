f956afc265485e79acb353c6624324bc59ea7c42
VuVanNghia <vuvannghia.work@gmail.com>
2026-03-17 02:01:07 +0700

    fix: update endpoint URL for upstash qstash topic

--- modules/upstash/main.tf
+++ modules/upstash/main.tf
@@ -2,7 +2,8 @@ resource "upstash_qstash_topic_v2" "events" {
   name = "events"
 
   endpoints = [
-    "https://dev-countdown-queries-service.toeichust.me/api/event-listener"
+    "https://example.com",
+    # "https://dev-countdown-queries-service.toeichust.me/api/event-listener"
     # "https://countdown-queries-service.toeichust.me/api/event-listener",
   ]
 }
