f7aa05879d37ed61e371e3f112ab0e1ee60fb3b4
VuVanNghia <vuvannghia.work@gmail.com>
2026-03-16 23:20:16 +0700

    Thêm aiven với kafka

---
+++ modules/aiven/main.tf
@@ -0,0 +1,26 @@
+resource "aiven_project" "this" {
+  project   = "vuvannghia-by-terraform"
+  parent_id = var.doppler_secrets_map["AIVEN_PARENT_ID"]
+}
+
+
+resource "aiven_kafka" "kafka_srv" {
+  project      = aiven_project.this.project
+  cloud_name   = "do-sgp"
+  plan         = "free-0"
+  service_name = "kafka-by-terraform"
+
+
+  kafka_user_config {
+    kafka_version   = "4.1"
+    kafka_rest    = true
+    kafka_connect = false
+    schema_registry = true
+
+
+    public_access {
+      kafka_rest    = true
+      kafka_connect = false
+    }
+  }
+}
