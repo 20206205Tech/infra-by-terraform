28a7dc2582741d3d559ad83c77343c1f9dc036e1
VuVanNghia <vuvannghia.work@gmail.com>
2026-03-17 22:45:33 +0700

    fix: comment out unused project names in variables.tf

--- modules/neon/variables.tf
+++ modules/neon/variables.tf
@@ -8,7 +8,7 @@ variable "projects" {
   type        = list(string)
   description = "Danh sách tên các project Neon cần tạo"
   default = [
-    "dev-test-everything",
+    # "dev-test-everything",
 
     "dev-api-gateway",
     "prod-api-gateway",
@@ -28,7 +28,7 @@ variable "projects" {
     "dev-crawl-data-thuvienphapluat",
     "prod-crawl-data-thuvienphapluat",
   
-    "dev-data-pipeline-vbpl",
-    "prod-data-pipeline-vbpl",
+    # "dev-data-pipeline-vbpl",
+    # "prod-data-pipeline-vbpl",
   ]
 }
