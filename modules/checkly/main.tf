d99ed4a3a41b1b2167697b298e35ec48e219a2fc
vuvannghiawork <vuvannghia.work@gmail.com>
2026-03-18 21:28:23 +0700

    89 (#63)
    * 89
    
    * refactor: uncomment Checkly API check configurations for clarity

--- modules/checkly/main.tf
+++ modules/checkly/main.tf
@@ -1,37 +1,44 @@
-# 1. Check Ping API (GET)
-resource "checkly_check" "dsg1_api_check" {
-  name                      = "test-stacloud-vps-ping-api"
-  type                      = "API"
-  activated                 = true
-  frequency                 = 1440 
-  locations                 = ["ap-southeast-1"]
+# # 1. Check Ping API - Chạy chính xác vào 8:00 sáng hàng ngày (GMT+7)
+# resource "checkly_check" "dsg1_api_check" {
+#   name                      = "test-stacloud-vps-ping-api"
+#   type                      = "API"
+#   activated                 = true
+#   locations                 = ["ap-southeast-1"]
 
-  request {
-    url    = "http://dsg1.stacloud.tech:30005/docs"
-    method = "GET"
-  }
-}
+#   # Cấu hình lịch chạy Cron
+#   scheduling_strategy = "CRON"
+#   schedule            = "0 8 * * *" # Chạy vào phút 0, giờ 8 hàng ngày
 
-# 2. Check Trigger GitHub (POST)
-resource "checkly_check" "dsg1_trigger_check" {
-  name      = "test-stacloud-vps-trigger"
-  type      = "API"
-  activated = true
-  frequency = 1440
-  locations = ["ap-southeast-1"]
+#   request {
+#     url    = "http://dsg1.stacloud.tech:30005/docs"
+#     method = "GET"
+#   }
+# }
 
-  request {
-    url    = "https://api.github.com/repos/20206205Tech/test-stacloud-vps/dispatches"
-    method = "POST"
+# # 2. Check Trigger GitHub - Chạy chính xác vào 9:00 sáng hàng ngày (GMT+7)
+# resource "checkly_check" "dsg1_trigger_check" {
+#   name      = "test-stacloud-vps-trigger"
+#   type      = "API"
+#   activated = true
+#   locations = ["ap-southeast-1"]
+
+#   # Cấu hình lịch chạy Cron
+#   scheduling_strategy = "CRON"
+#   schedule            = "0 9 * * *" # Chạy vào phút 0, giờ 9 hàng ngày
+
+#   request {
+#     url    = "https://api.github.com/repos/20206205Tech/test-stacloud-vps/dispatches"
+#     method = "POST"
     
-    headers = {
-      "Authorization" = "Bearer ${var.doppler_secrets_map["PAT"]}"
-      "Accept"        = "application/vnd.github.v3+json"
-    }
+#     headers = {
+#       "Authorization" = "Bearer ${var.doppler_secrets_map["PAT"]}"
+#       "Accept"        = "application/vnd.github.v3+json"
+#       "Content-Type"  = "application/json"
+#     }
 
-    body = jsonencode({
-      "event_type" = "trigger"
-    })
-    body_type = "JSON"
-  }
-}
+#     body = jsonencode({
+#       "event_type" = "trigger"
+#     })
+#     body_type = "JSON"
+#   }
+# }
