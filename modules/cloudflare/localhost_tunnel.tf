7b642b824e12d9363832c19314d573771cde89e5
VuVanNghia <vuvannghia.work@gmail.com>
2026-03-22 22:45:06 +0700

    ollama

--- modules/cloudflare/localhost_tunnel.tf
+++ modules/cloudflare/localhost_tunnel.tf
@@ -1,4 +1,3 @@
-
 resource "cloudflare_zero_trust_tunnel_cloudflared" "localhost_tunnel" {
   account_id = var.doppler_secrets_map["CLOUDFLARE_ACCOUNT_ID"]
   name       = "localhost-tunnel"
@@ -17,15 +16,28 @@ resource "cloudflare_zero_trust_tunnel_cloudflared_config" "localhost_tunnel_con
     }
 
     ingress_rule {
+      hostname = "ollama.${var.domain_name}"
+      service  = "http://127.0.0.1:11434"
+    }
+
+    ingress_rule {
       service = "http_status:404"
     }
   }
 }
 
-# Bản ghi DNS trỏ về Tunnel
 resource "cloudflare_record" "localhost_tunnel" {
   zone_id = data.cloudflare_zone.domain.id
   name    = "dev-be-ai"
+  type    = "CNAME"
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.localhost_tunnel.id}.cfargotunnel.com"
+  proxied = true
+  ttl     = 1
+}
+
+resource "cloudflare_record" "ollama_tunnel" {
+  zone_id = data.cloudflare_zone.domain.id
+  name    = "ollama"
   type    = "CNAME"
   content = "${cloudflare_zero_trust_tunnel_cloudflared.localhost_tunnel.id}.cfargotunnel.com"
   proxied = true
