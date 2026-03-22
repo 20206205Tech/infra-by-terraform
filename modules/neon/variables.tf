a4c1a1548cabdd3d043705104d5fb4c65dd382fa
VuVanNghia <vuvannghia.work@gmail.com>
2026-03-22 10:51:39 +0700

    docs: update project list in variables.tf to include 'dev-everything' and 'prod-everything'

--- modules/neon/variables.tf
+++ modules/neon/variables.tf
@@ -8,13 +8,10 @@ variable "projects" {
   type        = list(string)
   description = "Danh sách tên các project Neon cần tạo"
   default = [
-    # "dev-everything",
-    # "prod-everything",
-
     "dev-api-gateway",
     "prod-api-gateway",
-    "dev-api-gateway-http-log",
-    "prod-api-gateway-http-log",
+    # "dev-api-gateway-http-log",
+    # "prod-api-gateway-http-log",
 
     "dev-be-ai",
     "prod-be-ai",
@@ -25,18 +22,21 @@ variable "projects" {
     "dev-crawl-data-luatvietnam",
     "prod-crawl-data-luatvietnam",
 
-    # "dev-crawl-data-thuvienphapluat",
-    # "prod-crawl-data-thuvienphapluat",
-
-    "dev-data-pipeline-vbpl",
-    "prod-data-pipeline-vbpl",
 
 
 
-    "dev-vector-database",
-    "prod-vector-database",
+
+    # "dev-data-pipeline-vbpl",
+    # "prod-data-pipeline-vbpl",
 
 
+
+    # "dev-vector-database",
+    # "prod-vector-database",
+
+
+    "dev-everything",
+    "prod-everything",
 
   ]
 }
