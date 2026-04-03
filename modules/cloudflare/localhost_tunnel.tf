61b3731d9ab0e09b3f149c75252b8dfbb500f109
vuvannghiawork <vuvannghia.work@gmail.com>
2026-04-03 22:49:12 +0700

    dev-data-pipeline-service (#111)

--- modules/cloudflare/localhost_tunnel.tf
+++ modules/cloudflare/localhost_tunnel.tf
@@ -20,15 +20,30 @@ resource "cloudflare_zero_trust_tunnel_cloudflared_config" "localhost_tunnel_con
     }
 
     ingress_rule {
+      hostname = "dev-data-pipeline-service.${var.domain_name}"
+      service  = "http://localhost:30001"
+    }
+
+    ingress_rule {
       service = "http_status:404"
     }
   }
 }
 
-# Bản ghi DNS trỏ về Tunnel
+
+
 resource "cloudflare_record" "localhost_tunnel" {
   zone_id = data.cloudflare_zone.domain.id
   name    = "dev-be-ai"
+  type    = "CNAME"
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.localhost_tunnel.id}.cfargotunnel.com"
+  proxied = true
+  ttl     = 1
+}
+
+resource "cloudflare_record" "data_pipeline_tunnel_dns" {
+  zone_id = data.cloudflare_zone.domain.id
+  name    = "dev-data-pipeline-service"  
   type    = "CNAME"
   content = "${cloudflare_zero_trust_tunnel_cloudflared.localhost_tunnel.id}.cfargotunnel.com"
   proxied = true
