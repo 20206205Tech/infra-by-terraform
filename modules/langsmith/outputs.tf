e5ae69b096cc88b163cc90881235dc0653e09b9c
VuVanNghia <vuvannghia.work@gmail.com>
2026-04-08 13:52:28 +0700

    tắt

--- modules/langsmith/outputs.tf
+++ modules/langsmith/outputs.tf
@@ -1,4 +1,4 @@
-output "project_ids" {
-  description = "Danh sách ID của các project đã được tạo"
-  value       = { for k, project in langsmith_project.this : k => project.id }
-}
+# output "project_ids" {
+#   description = "Danh sách ID của các project đã được tạo"
+#   value       = { for k, project in langsmith_project.this : k => project.id }
+# }
