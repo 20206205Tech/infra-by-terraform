6d8b77206f87c054bb9f6db2a548772ed88e13af
VuVanNghia <vuvannghia.work@gmail.com>
2026-04-17 20:33:36 +0700

    conversation + payment

--- modules/cloudflare/localhost_tunnel.tf
+++ modules/cloudflare/localhost_tunnel.tf
@@ -23,14 +23,14 @@ resource "cloudflare_zero_trust_tunnel_cloudflared_config" "localhost_tunnel_con
       hostname = "dev-data-pipeline-service.${var.domain_name}"
       service  = "http://localhost:30001"
     }   
-     ingress_rule {
-      hostname = "dev-code-persona-service.${var.domain_name}"
-      service  = "http://localhost:30002"
-    }  
-     ingress_rule {
-      hostname = "dev-code-setting-service.${var.domain_name}"
-      service  = "http://localhost:30003"
-    }
+    #  ingress_rule {
+    #   hostname = "dev-code-persona-service.${var.domain_name}"
+    #   service  = "http://localhost:30002"
+    # }  
+    #  ingress_rule {
+    #   hostname = "dev-code-setting-service.${var.domain_name}"
+    #   service  = "http://localhost:30003"
+    # }
      ingress_rule {
       hostname = "dev-code-subscription-service.${var.domain_name}"
       service  = "http://localhost:30004"
@@ -42,6 +42,14 @@ resource "cloudflare_zero_trust_tunnel_cloudflared_config" "localhost_tunnel_con
      ingress_rule {
       hostname = "dev-code-chatbot-service.${var.domain_name}"
       service  = "http://localhost:30006"
+    }
+     ingress_rule {
+      hostname = "dev-code-conversation-service.${var.domain_name}"
+      service  = "http://localhost:30008"
+    }
+     ingress_rule {
+      hostname = "dev-code-payment-service.${var.domain_name}"
+      service  = "http://localhost:30009"
     }
 
     ingress_rule {
@@ -70,23 +78,23 @@ resource "cloudflare_record" "data_pipeline_tunnel_dns" {
   ttl     = 1
 }
 
-resource "cloudflare_record" "persona_tunnel_dns" {
-  zone_id = data.cloudflare_zone.domain.id
-  name    = "dev-code-persona-service"
-  type    = "CNAME"
-  content = "${cloudflare_zero_trust_tunnel_cloudflared.localhost_tunnel.id}.cfargotunnel.com"
-  proxied = true
-  ttl     = 1
-}
+# resource "cloudflare_record" "persona_tunnel_dns" {
+#   zone_id = data.cloudflare_zone.domain.id
+#   name    = "dev-code-persona-service"
+#   type    = "CNAME"
+#   content = "${cloudflare_zero_trust_tunnel_cloudflared.localhost_tunnel.id}.cfargotunnel.com"
+#   proxied = true
+#   ttl     = 1
+# }
 
-resource "cloudflare_record" "setting_tunnel_dns" {
-  zone_id = data.cloudflare_zone.domain.id
-  name    = "dev-code-setting-service"
-  type    = "CNAME"
-  content = "${cloudflare_zero_trust_tunnel_cloudflared.localhost_tunnel.id}.cfargotunnel.com"
-  proxied = true
-  ttl     = 1
-}
+# resource "cloudflare_record" "setting_tunnel_dns" {
+#   zone_id = data.cloudflare_zone.domain.id
+#   name    = "dev-code-setting-service"
+#   type    = "CNAME"
+#   content = "${cloudflare_zero_trust_tunnel_cloudflared.localhost_tunnel.id}.cfargotunnel.com"
+#   proxied = true
+#   ttl     = 1
+# }
 
 resource "cloudflare_record" "subscription_tunnel_dns" {
   zone_id = data.cloudflare_zone.domain.id
@@ -109,6 +117,26 @@ resource "cloudflare_record" "document_tunnel_dns" {
 resource "cloudflare_record" "chatbot_tunnel_dns" {
   zone_id = data.cloudflare_zone.domain.id
   name    = "dev-code-chatbot-service"
+  type    = "CNAME"
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.localhost_tunnel.id}.cfargotunnel.com"
+  proxied = true
+  ttl     = 1
+}
+
+
+
+resource "cloudflare_record" "conversation_tunnel_dns" {
+  zone_id = data.cloudflare_zone.domain.id
+  name    = "dev-code-conversation-service"
+  type    = "CNAME"
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.localhost_tunnel.id}.cfargotunnel.com"
+  proxied = true
+  ttl     = 1
+}
+
+resource "cloudflare_record" "payment_tunnel_dns" {
+  zone_id = data.cloudflare_zone.domain.id
+  name    = "dev-code-payment-service"
   type    = "CNAME"
   content = "${cloudflare_zero_trust_tunnel_cloudflared.localhost_tunnel.id}.cfargotunnel.com"
   proxied = true
