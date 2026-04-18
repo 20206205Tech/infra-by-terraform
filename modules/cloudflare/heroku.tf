3c11d88e34c76ffe9a26b27004d8a2797ee43964
VuVanNghia <vuvannghia.work@gmail.com>
2026-04-18 18:18:42 +0700

    code-payment-service

--- modules/cloudflare/heroku.tf
+++ modules/cloudflare/heroku.tf
@@ -1,9 +1,7 @@
-
-
 # Bản ghi DNS cho Heroku
 resource "cloudflare_record" "be_ai_heroku" {
   zone_id = data.cloudflare_zone.domain.id
-  name    = "be-ai"
+  name    = "code-payment-service"
   type    = "CNAME"
   content = var.heroku_dns_target
   proxied = true
